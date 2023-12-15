15a92dc3b84f0b3afad093d7e5ffc3ed26ddc0c0
Pranav Sawant <pranavsawant2579@gmail.com>
2023-12-15 23:29:28 +0530

    Changed the chatpage design to better

--- ChatBot/consumers.py
+++ ChatBot/consumers.py
@@ -27,9 +27,9 @@ class ChatConsumer(WebsocketConsumer):
             # Initialize conversation chain on first message
             pdf_details = get_object_or_404(PDF_Details, pdf_id=6)
             vectordb_path = pdf_details.pdf_vectordb_path
-            embedding = OpenAIEmbeddings(openai_api_key='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')
+            embedding = OpenAIEmbeddings(openai_api_key='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')
             vectordb = Chroma(persist_directory=vectordb_path, embedding_function=embedding)
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0.6,openai_api_key='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')
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0.6,openai_api_key='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')
             memory = ConversationBufferMemory(memory_key="chat_history",return_messages = True)
             retriever = vectordb.as_retriever()
             self.qa = ConversationalRetrievalChain.from_llm(llm,retriever = retriever,memory = memory)
@@ -44,3 +44,30 @@ class ChatConsumer(WebsocketConsumer):
             'question':message,
             'message':result["answer"]
         }))
+
+        def createPersona(self, ch):
+            gen_prompt = '''
+                            You are a general assistant AI chatbot here to assist the user based on the PDFs they uploaded,
+                            and the subsequent openAI embeddings. Please assist the user to the best of your knowledge based on 
+                            uploads, embeddings and the following user input. USER INPUT: 
+                        '''
+
+            acc_prompt = '''
+                            You are a academic assistant AI chatbot here to assist the user based on the academic PDFs they uploaded,
+                            and the subsequent openAI embeddings. This academic persona allows you to use as much outside academic responses as you can.
+                            But remember this is an app for academix PDF question. Please respond in as academic a way as possible, with an academix audience in mind
+                            Please assist the user to the best of your knowledge, with this academic persona
+                            based on uploads, embeddings and the following user input. USER INPUT: 
+                        '''
+
+            witty_prompt = '''
+                            You are a witty assistant AI chatbot here to assist the user based on the PDFs they uploaded,
+                            and the subsequent openAI embeddings. This witty persona should make you come off as lighthearted,
+                            be joking responses and original, with the original user question still being answered.
+                            Please assist the user to the best of your knowledge, with this comedic persona
+                            based on uploads, embeddings and the following user input. USER INPUT: 
+                        '''
+            if ch==1: prompt = gen_prompt
+            elif ch==2: prompt = acc_prompt
+            elif ch==3: prompt = witty_prompt
+            return prompt
